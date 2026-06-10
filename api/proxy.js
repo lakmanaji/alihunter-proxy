0d7fdc1b58c2fd379d9464be2e2cfac9df6464eb
lakmanaji <lakmafor@gmail.com>
2026-06-10 10:38:48 +0100

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  try {
+    const params = req.query;
+    const url = `https://api-sg.aliexpress.com/sync?${new URLSearchParams(params)}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
